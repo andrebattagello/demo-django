1ca79a003d5d27660824f7c76c469ed0a41b96d1
Rafael Machado <rfa1989@gmail.com>
2012-03-25 15:42:48 -0300

    ver lista spec tests -> still red

--- src/apps/lista/tests/views_spec.py
+++ src/apps/lista/tests/views_spec.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
+from django.test import Client
+from lista.models import Lista
 
 
 class VerListaSpec(TestCase):
 
+    def setUp(self):
+        self.client = Client()
+
     def deve_saber_mostrar_lista_vazia(self):
-        assert True
+        Lista()
+        response = self.client.post('/lista/')
+        self.assertEqual(200, response.status_code)
+        self.assertContains(response, 'Lista Vazia')
