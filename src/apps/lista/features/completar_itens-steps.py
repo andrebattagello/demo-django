24015318c08b3587f8070a3668c105bc4592ca5d
Rafael Machado <rfa1989@gmail.com>
2012-03-30 16:33:48 -0300

    completar itens features: python steps

---
+++ src/apps/lista/features/completar_itens-steps.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from lettuce import step, world
+from lettuce.django import django_url
+#from lista.models import Item
+
+
+@step(u'Quando eu marcar o "([^"]*)" como completo')
+def quando_eu_marcar_o_item_como_completo(step, item_name):
+    world.browser.visit(django_url('/lista/'))
+    world.browser.click_link_by_partial_text(item_name)
+    world.browser.find_by_name('completo').first.check()
+
+
+@step(u'Então eu devo verificar que o "([^"]*)" realmente está completo')
+def entao_eu_devo_verificar_que_o_item_realmente_esta_completo(step,
+                                                               item_name):
+    world.browser.visit(django_url('/lista/'))
+    assert "%s ==> Completo" % item_name
