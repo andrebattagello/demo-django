b602dd4dfa32349c0534412c229c8931d2544a14
Rafael Machado <rfa1989@gmail.com>
2012-03-30 16:37:41 -0300

    model impl

--- src/apps/lista/models.py
+++ src/apps/lista/models.py
@@ -8,3 +8,15 @@ class Item(models.Model):
     """
     nome = models.CharField(max_length=512, blank=False)
     completo = models.BooleanField(default=False)
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('ver_detalhes_item', (), {
+            'item_id': self.id
+        })
+
+    @property
+    def completo_texto(self):
+        if self.completo:
+            return 'Completo'
+        return 'Incompleto'
