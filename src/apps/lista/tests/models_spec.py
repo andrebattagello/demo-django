59ebfd9c8a026b9353ecc92d2afbb642321a352e
Rafael Machado <rfa1989@gmail.com>
2012-03-30 16:37:29 -0300

    new model tests

--- src/apps/lista/tests/models_spec.py
+++ src/apps/lista/tests/models_spec.py
@@ -15,11 +15,22 @@ class ItemSpec(TestCase):  # Specification
     """
     Comportamento do model Item
     """
+    def setUp(self):
+        self.item = Item(nome='Item 1')
+        self.item.save()
+
     def deve_ter_um_nome(self):
-        item = Item(nome='Item 1')
-        item.save()
-        assert_equals(item.nome, 'Item 1')
+        assert_equals(self.item.nome, 'Item 1')
 
     def deve_comecar_nao_completo(self):
-        item = Item(nome='Item 1')
-        assert item.completo is False
+        assert self.item.completo is False
+
+    def deve_saber_a_sua_url_absoluta(self):
+        assert_equals(self.item.get_absolute_url(),
+                      '/lista/%s/' % self.item.id)
+
+    def deve_saber_traduzir_para_texto_se_esta_completo(self):
+        self.item.completo = 0
+        assert_equals(self.item.completo_texto, 'Incompleto')
+        self.item.completo = 1
+        assert_equals(self.item.completo_texto, 'Completo')
